21e4499e3aaeb17c6dedcdd5736a60d149f251ef
Gawasp <130310125+Gawasp@users.noreply.github.com>
2024-03-29 13:55:39 +0000

    2nd commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('Hello, World!!');
   });
+
+app.get('/api/auth/signup',(req,res) => {
+  console.log("signup route");
+});
+
 
 app.listen(PORT, () =>  console.log(`Server is running on port ${PORT}`));
